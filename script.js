ceb1475c2a6560b4fa7a5e0815e480727f27f641
Aleksandr Moruga <rnoruga@gmail.com>
2026-08-01 23:32:39 +0300

    Get computer game choice

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello World!")
+function getComputerChoice() { 
+let randomInt = Math.floor(Math.random() * 3); 
+
+switch(randomInt) 
+{ 
+case 0: return "rock"; 
+case 1: return "paper";
+case 2: return "scissors";
+} 
+}
+
+console.log(getComputerChoice())
