5a53dcc1533acb9f2214dfebd753f65491100f41
Aleksandr Moruga <rnoruga@gmail.com>
2026-08-01 23:52:05 +0300

    Get human choice

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function getComputerChoice() { 
 let randomInt = Math.floor(Math.random() * 3); 
 
-switch(randomInt) 
-{ 
+switch(randomInt) { 
 case 0: return "rock"; 
 case 1: return "paper";
-case 2: return "scissors";
-} 
+case 2: return "scissors"; } 
 }
 
 console.log(getComputerChoice())
+
+function getHumanChoice() {
+    return prompt("rock, paper or scissors?")
+}
+
+console.log(getHumanChoice())
