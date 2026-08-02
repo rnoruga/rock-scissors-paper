e55b495619ea8779d9175415bd481733369413b3
Aleksandr Moruga <rnoruga@gmail.com>
2026-08-02 13:17:45 +0300

    Implement single round play

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let computerScore = 0
+let humanScore = 0
+
 function getComputerChoice() { 
 let randomInt = Math.floor(Math.random() * 3); 
 
@@ -7,10 +10,35 @@ case 1: return "paper";
 case 2: return "scissors"; } 
 }
 
-console.log(getComputerChoice())
+// console.log(getComputerChoice())
 
 function getHumanChoice() {
     return prompt("rock, paper or scissors?")
 }
 
-console.log(getHumanChoice())
+// console.log(getHumanChoice())
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase()
+
+    if (humanChoice === computerChoice) {
+        console.log("Draw! Nobody wins");
+        console.log(`Human: ${humanScore}`);
+        console.log(`Computer: ${computerScore}`);
+}   else if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper")) {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+        ++humanScore;
+        console.log(`Human: ${humanScore}`);
+        console.log(`Computer: ${computerScore}`);
+}   else {
+    console.log(`You lost! ${computerChoice} beats ${humanChoice}.`);
+    ++computerScore;
+    console.log(`Human: ${humanScore}`);
+    console.log(`Computer: ${computerScore}`);
+}
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
